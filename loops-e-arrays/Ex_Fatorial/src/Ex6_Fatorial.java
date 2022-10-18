6e1c3118eedea3304740bbd607121a69449d7fdb
mainartijonathas <mainartidias@yahoo.com.br>
2022-10-18 10:47:41 -0300

    Ex6_Fatorial

---
+++ loops-e-arrays/Ex_Fatorial/src/Ex6_Fatorial.java
@@ -0,0 +1,23 @@
+import java.security.Signature;
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex: 5! = 120 (5x4x3x2x1)
+ */
+public class Ex6_Fatorial{
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial de: ");
+        int fatorial = scan.nextInt();
+
+        int multiplicacao = 1;
+
+        System.out.println(fatorial + "! = ");
+        for (int i = fatorial ; i >= 1 ; i-- ){
+            multiplicacao = multiplicacao * i;
+        }
+        System.out.println(multiplicacao);
+    }
+}
