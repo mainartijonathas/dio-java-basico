209191df3d77ab7828f094adec732c663b0f2970
mainartijonathas <mainartidias@yahoo.com.br>
2022-11-14 10:32:47 -0300

    exercício resolvido

---
+++ Metodos/Sobrecarga/Area Quadrilateros/Quadrilatero_retorno/src/Quadrilatero.java
@@ -0,0 +1,18 @@
+/*
+ Classe com os métodos que calculam a área dos quadrilateros, Quadrado, Retangulo e Trapézio
+ */
+
+public class Quadrilatero {
+    public static double area(double lado) {
+      return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+        return ((baseMaior + baseMenor) * altura) / 2;
+    }
+
+}
