ac714ed55a5e5a2b1c5c3d1570c1644003216159
mainartijonathas <mainartidias@yahoo.com.br>
2022-12-07 17:29:04 -0300

    realizado

---
+++ Collections/Exercício List/src/ExemploList.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class ExemploList {
+    
+    public static void main(String[] args) {
+      
+        // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça:
+
+//      List notas = new ArrayList(); //antes do java 5
+//      ArrayList<Double> notas = new ArrayList<>();
+//      List<Double> notas = new ArrayList<>(Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6));
+        /*List<Double> notas = Arrays.asList(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        notas.add(10d);
+        System.out.println(notas);*/
+        /*List<Double> notas = List.of(7d, 8.5, 9.3, 5d, 7d, 0d, 3.6);
+        notas.add(1d);
+        notas.remove(5d);
+        System.out.println(notas);*/
+
+        System.out.println("Crie uma lista e adicione as sete notas: ");
+
+        List<Double> notas = new ArrayList<Double>();
+        notas.add(7.0);
+        notas.add(8.5);
+        notas.add(9.3);
+        notas.add(5.0);
+        notas.add(0.0);
+        notas.add(3.6);
+
+        System.out.println(notas);
+
+        System.out.println("Exiba a posição da nota 5.0: " + notas.indexOf(5d)); // lista.indexOf retornar em qual posição está o termo declarado dento dos parenteses
+
+        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+        // para inserirmo um elemento na lista usamos lista.add(posição, item a ser inserido)
+        notas.add(4, 8d);
+        System.out.println(notas);
+
+
+    }
+}
